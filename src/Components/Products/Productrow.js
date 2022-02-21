9c25cd92767e92e0fdf7aa79afd93256b68b1b52
Sudhir <sudhir@livelgam.com>
2022-02-21 23:27:13 +0530

    create masonry

--- src/Components/Products/Productrow.js
+++ src/Components/Products/Productrow.js
@@ -1,15 +1,23 @@
-import React, { Component } from 'react';
-import { Row } from 'react-bootstrap';
+import React, { Component } from "react";
+import { Row } from "react-bootstrap";
+import Masonry from "react-masonry-css";
+import './ProductRow.css';
 
 class Productrow extends Component {
-
-    render(props) {
-        return (
-                <Row className="my-5">
-                    { this.props.children }
-          </Row>
-        );
-    }
+	render(props) {
+		return (
+			// <Row>
+			// 	{this.props.children}
+			// </Row>
+			<Masonry
+				breakpointCols={4}
+				className="my-masonry-grid card-group"
+				columnClassName="my-masonry-grid_column"
+			>
+				{this.props.children}
+			</Masonry>
+		);
+	}
 }
 
 export default Productrow;
