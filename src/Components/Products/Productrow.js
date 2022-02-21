7bc6ed698274e276bd0c073f1fdeabc451d638ea
Sudhir <sudhir@livelgam.com>
2022-02-22 02:01:36 +0530

    add masonry link

--- src/Components/Products/Productrow.js
+++ src/Components/Products/Productrow.js
@@ -3,14 +3,19 @@ import { Row } from "react-bootstrap";
 import Masonry from "react-masonry-css";
 import './ProductRow.css';
 
+const breakpointColumnsObj = {
+    default: 4,
+    1100: 3,
+    700: 2,
+    500: 1
+  };
+
 class Productrow extends Component {
 	render(props) {
 		return (
-			// <Row>
-			// 	{this.props.children}
-			// </Row>
+			// using masonry https://thewebdev.info/2020/09/04/add-a-masonry-grid-to-a-react-app-with-the-react-masonry-css-library/
 			<Masonry
-				breakpointCols={4}
+				breakpointCols={breakpointColumnsObj}
 				className="my-masonry-grid card-group"
 				columnClassName="my-masonry-grid_column"
 			>
