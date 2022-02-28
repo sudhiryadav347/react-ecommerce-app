dc9dd3ae0cafece9e879b126f9c2cf53fb10f78f
Sudhir <sudhir@livelgam.com>
2022-03-01 00:47:14 +0530

    #22 use context api to share islogged in data with navigation component

--- src/Components/UI/NavigationBar.js
+++ src/Components/UI/NavigationBar.js
@@ -1,79 +1,87 @@
 import React, { Fragment } from "react";
 import { NavLink } from "react-router-dom";
+import AuthContext from "../Context/auth-context";
 
 const Navigationbar = (props) => {
 	return (
-		<>
-			<nav className="navbar navbar-expand-lg navbar-light bg-light">
-				<div className="container">
-					<button
-						className="navbar-toggler"
-						type="button"
-						data-bs-toggle="collapse"
-						data-bs-target="#navbarSupportedContent"
-						aria-controls="navbarSupportedContent"
-						aria-expanded="false"
-						aria-label="Toggle navigation"
-					>
-						<span className="navbar-toggler-icon"></span>
-					</button>
-					<div className="collapse navbar-collapse" id="navbarSupportedContent">
-						<ul className="navbar-nav me-auto mb-2 mb-lg-0">
-							<li className="nav-item">
-								<NavLink to="/" className="nav-link">
-									Home
-								</NavLink>
-							</li>
-							<li className="nav-item">
-								<NavLink to="/about" className="nav-link">
-									About
-								</NavLink>
-							</li>
-						</ul>
-
-						<ul className="navbar-nav me-4 mb-2 mb-lg-0">
-							{props.whetherLoggedIn ? (
-								<React.Fragment>
-									<li className="nav-item">
-										<NavLink to="/dashboard" className="nav-link">
-											Dashboard
-										</NavLink>
-									</li>
-
-									<li className="nav-item">
-										<NavLink
-											to="/"
-											className="nav-link"
-											onClick={props.onLogout}
-										>
-											Logout
-										</NavLink>
-									</li>
-								</React.Fragment>
-							) : (
-								<li className="nav-item">
-									<NavLink to="/login" className="nav-link">
-										Login
-									</NavLink>
-								</li>
-							)}
-						</ul>
-
-						<form className="d-flex">
-							<input
-								className="form-control me-2"
-								type="search"
-								placeholder="Search"
-								aria-label="Search"
-							/>
-							<button className="btn btn-outline-success" type="submit">
-								Search
+		<AuthContext.Consumer>
+			{(ctx) => {
+				return (
+					<nav className="navbar navbar-expand-lg navbar-light bg-light">
+						<div className="container">
+							<button
+								className="navbar-toggler"
+								type="button"
+								data-bs-toggle="collapse"
+								data-bs-target="#navbarSupportedContent"
+								aria-controls="navbarSupportedContent"
+								aria-expanded="false"
+								aria-label="Toggle navigation"
+							>
+								<span className="navbar-toggler-icon"></span>
 							</button>
-						</form>
-					</div>
-				</div>
-			</nav>
-		</>
+							<div
+								className="collapse navbar-collapse"
+								id="navbarSupportedContent"
+							>
+								<ul className="navbar-nav me-auto mb-2 mb-lg-0">
+									<li className="nav-item">
+										<NavLink to="/" className="nav-link">
+											Home
+										</NavLink>
+									</li>
+									<li className="nav-item">
+										<NavLink to="/about" className="nav-link">
+											About
+										</NavLink>
+									</li>
+								</ul>
+
+								<ul className="navbar-nav me-4 mb-2 mb-lg-0">
+									{ctx.isLoggedIn ? (
+										<React.Fragment>
+											<li className="nav-item">
+												<NavLink to="/dashboard" className="nav-link">
+													Dashboard
+												</NavLink>
+											</li>
+
+											<li className="nav-item">
+												<NavLink
+													to="/"
+													className="nav-link"
+													onClick={props.onLogout}
+												>
+													Logout
+												</NavLink>
+											</li>
+										</React.Fragment>
+									) : (
+										<li className="nav-item">
+											<NavLink to="/login" className="nav-link">
+												Login
+											</NavLink>
+										</li>
+									)}
+								</ul>
+
+								<form className="d-flex">
+									<input
+										className="form-control me-2"
+										type="search"
+										placeholder="Search"
+										aria-label="Search"
+									/>
+									<button className="btn btn-outline-success" type="submit">
+										Search
+									</button>
+								</form>
+							</div>
+						</div>
+					</nav>
+				);
+			}}
+		</AuthContext.Consumer>
 	);
 };
 
