857368b1c22bc9fa619c1db34c44e30c3eec3d8a
Sudhir Yadav <sudhiryadav347@gmail.com>
2022-03-06 18:02:57 +0530

    #36 Fix alert dismissible issue.

--- src/Components/Alerts/DismissableAlert.js
+++ src/Components/Alerts/DismissableAlert.js
@@ -1,21 +1,12 @@
-import React, { useEffect, useState } from 'react';
 import { Alert } from 'react-bootstrap';
 
 export default function DismissableAlert(props) {
-  const [Show, setShow] = useState(false);
-  // TODO:Unable to use dismissible property in the Alert component.
-  // Upon closing the alert the Show state gets set to false & new alert
-  // fails to appear.
-
-  useEffect(() => {
-    setShow(props.showAlert);
-  }, [props.showAlert]);
 
   return (
     <>
-      {Show && (
+      {props.showAlert && (
         <Alert
-          onClose={() => setShow(false)}
+          onClose={props.closeAlert}
           variant={props.variant}
           dismissible={props.dismissible}
         >
